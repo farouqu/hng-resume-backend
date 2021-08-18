ffe2a3e4e67021302a1dac6e49626908c28d1074
Farouq Umar <ufakz99@gmail.com>
2021-08-18 23:06:29 +0100

    feat: resume backend with form

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get("/", (req, res) => res.send("Hello World"));
+app.post("/submit-form", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+
+  res.status(200).send({result: "Mail successfully received"});
+
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server listening at ${port}`));
